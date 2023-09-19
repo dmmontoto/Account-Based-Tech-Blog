62382a40ba6db7e2bd7664c7e669d908114af553
dmmontoto <dmontoto@ucsd.edu>
2023-09-19 11:00:48 -0700

    added profile js

---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#post-name').value.trim();
+    const post = document.querySelector('#post').value.trim();
+    const description = document.querySelector('#post-desc').value.trim();
+  
+    if (name && post && description) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ name, post, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);  
