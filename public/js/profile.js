c69d8cba439a21c0cde8b87398985c0e9c6f2fdf
dmmontoto <dmontoto@ucsd.edu>
2023-09-19 16:41:12 -0700

    added seed data for posts and users

--- public/js/profile.js
+++ public/js/profile.js
@@ -33,7 +33,7 @@ const newFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to delete project');
+        alert('Failed to delete post');
       }
     }
   };
