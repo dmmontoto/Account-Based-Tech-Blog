d4f17e707f1e7b4fe9b9cc6cdaff561a6f299072
dmmontoto <dmontoto@ucsd.edu>
2023-09-19 17:13:15 -0700

    added post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./postRoutes');
+const projectRoutes = require('./postRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
