57e506de10af08fb8cafd853da283c610d3379b6
dmmontoto <dmontoto@ucsd.edu>
2023-09-23 20:36:22 -0700

    comments now have date added

--- models/Comment.js
+++ models/Comment.js
@@ -14,6 +14,11 @@ Comment.init(
     description: {
       type: DataTypes.TEXT,
     },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
